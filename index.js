f2c95b7b24d60251f27aca2e19e9691bd09e0e9a
Muhammad Sidiq Putra <sidiq.putra@tiket.com>
2021-06-24 18:56:39 +0700

    add Procfile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var Products = require("./product.json");
+var PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send("simple rest app");
@@ -16,6 +17,6 @@ app.get("/product/:id", (req, res) => {
     }))  
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
